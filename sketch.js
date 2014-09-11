7418146405cd840fc90fabb9b8f5de7023c7a8b6
Hajinsol <jinsor5542@gmail.com>
2014-09-11 14:24:29 +0900

    stroke

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
 function setup() {
   createCanvas(640, 480);
   background(01, 122, 183);
-  stroke(255);
+  
   noFill();
+
+  stroke(255, 0, 0);
   line(10, 10, 90, 10);
+
+  stroke(209, 122, 183);
     rect(20, 20, 80, 50);
+
+    stroke(24, 234, 102);
     ellipse(50, 50, 50, 50);
 }
