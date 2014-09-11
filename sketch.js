7e2ff00ef86475186918d64204f4f99703fe99c2
Hajinsol <jinsor5542@gmail.com>
2014-09-11 14:41:58 +0900

    fill

--- sketch.js
+++ sketch.js
@@ -2,16 +2,16 @@ function setup() {
   createCanvas(640, 480);
   background(01, 122, 183);
   
-  //noFill();
+  noFill();
 
     stroke(255, 0, 0);
   line(10, 10, 90, 10);
 
   stroke(209, 122, 183);
-  Fill(209, 122, 183);
+  fill(209, 122, 183);
     rect(20, 20, 80, 50);
 
     stroke(24, 234, 102);
-    Fill(217, 224, 33);
+    fill(217, 224, 33);
     ellipse(50, 50, 50, 50);
 }
